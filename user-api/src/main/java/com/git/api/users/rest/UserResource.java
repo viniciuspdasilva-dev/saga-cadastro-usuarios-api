c8675dcbaad4b2508a46198856c7b9333d82a0de
Vinicius Silva <viniciuspdasilva.dev@gmail.com>
2025-07-05 20:48:16 -0300

    ADD - Implements new feature from a user added with Kafka producer

---
+++ user-api/src/main/java/com/git/api/users/rest/UserResource.java
@@ -0,0 +1,11 @@
+package com.git.api.users.rest;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.Path;
+
+@ApplicationPath( "/users")
+@ApplicationScoped
+public class UserResource {
+}
--- user-api/src/main/java/com/git/api/users/services/impl/NotificationServiceImpl.java
+++ user-api/src/main/java/com/git/api/users/services/impl/NotificationServiceImpl.java
@@ -2,11 +2,9 @@ package com.git.api.users.services.impl;
 
 import com.git.api.users.providers.kafka.ProducerService;
 import com.git.api.users.services.NotificationService;
-import com.git.api.users.services.UserService;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.infrastructure.Infrastructure;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.inject.spi.Producer;
 
 @ApplicationScoped
 public class NotificationServiceImpl implements NotificationService {
